e0c840cdd71ab91132d3a5fad89765a7eafb8ae2
Marina Kovaleva <sunshining1106@mail.ru>
2023-01-18 17:19:24 +0300

    Add section Portfolio

--- js/script.js
+++ js/script.js
@@ -4,5 +4,18 @@ $(document).ready(function(){
         $('.icon-menu, .menu__body').toggleClass('menu-open');
         $('body'),toggleClass('lock');
     });
+
+    new Swiper('.portfolio__slider', {
+        //Arrows
+        slidesPerView: 2,
+        navigation: {
+            nextEl: '.portfolio__arrow_right',
+            prevEl:'.portfolio__arrow_left'
+        }
+    });
 });
+
+
+
+
 
